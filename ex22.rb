bfc5fe5c478ff057db888661a975ba018c04d19a
jlharsenal <jonnyhale@gmail.com>
2015-10-21 17:32:29 +0100

    Create ex22.rb

---
+++ ex22.rb
@@ -0,0 +1,28 @@
+Word & Symbol (Character): Definition
+
+puts    : prints out statement (Prints out two different string on separate lines vs. concatenating them together) 
+print   : prints out statement (Concatenates two different strings together vs. printing them out on separate lines)
+#       : comment
++       : class: fixnum, performs addition 
+-       : class: fixnum, performs subtraction
+/       : class: fixnum, performs division 
+*       : class: fixnum, performs multiplication 
+%       : class: fixnum, performs modulus 
+<       : class: fixnum, returns true if value of fix is less than that of real 
+>       : class: fixnum, returns true if the value of fix is greater than of real 
+<=      : class: fixnum, returns true if the value of fix is less than or equal to that of real 
+>=      : class: fixnum, returns true if th value of fix is greater than or less than real 
+gets    : class: io, get (string) input from user 
+chomp : class: fixnum, gets rid of any space after the input is gotten from the user
+include : takes all the methods from another module and includes them in the current module 
+require : runs another file; it tracks what you've required and wont do require it 2x; this is a library that gets included. 
+ARGV    : argument variable that's a constant; variable holds the arguments you pass to your Ruby script when you run it; the name of the script that you run is stored in a special variable $0
+truncate: cuts off the file by a certain amt.
+write:  : writes to the file
+open    : opens file
+close   : closes file
+read    : reads a file
+seek    : class: io, seeks to the offset amount 
+def     : used for defining new functions
+seek_set: part of the io library, seeks to the absolute location given the amount
+rewind  : positions the file to the beginning of input, resetting line# to zero
